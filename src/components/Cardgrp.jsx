7e3d7a05640ec8f6eac90a154317ccc34a2a78ac
Prathamesh2801 <truepratham2801@gmail.com>
2024-10-17 16:54:04 +0530

    Added Home and Card Section

---
+++ src/components/Cardgrp.jsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+import CardItem from './CardItem'
+
+export default function Cardgrp() {
+  return (
+    <div className='bg-teal-800  min-h-[80vh] grid-rows-3'>
+        <div className='col-auto'>
+
+        <CardItem/>
+      
+        </div>
+      
+    </div>
+  )
+}
